38641cdd3928a849cc832fa8a85c9b503134472f
sehdev <sehdev@atai.ai>
2024-07-24 10:54:40 +0530

    Same position then use and preserve the state even for multiple instances of component on re render

---
+++ preserves-state/src/App.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import './App.css';
+export default function App() {
+  const [isFancy, setIsFancy] = useState(false);
+  return (
+    // React Fiber Algo Take care to use same value if components are same position
+    <div>
+      {isFancy ? (
+        <Counter isFancy={true} />
+      ) : (
+        <Counter isFancy={false} />
+      )}
+      <label>
+        <input
+          type="checkbox"
+          checked={isFancy}
+          onChange={e => {
+            setIsFancy(e.target.checked)
+          }}
+        />
+        Use fancy styling
+      </label>
+    </div>
+  );
+}
+
+function Counter({ isFancy }: {isFancy:boolean}) {
+  const [score, setScore] = useState(0);
+  const [hover, setHover] = useState(false);
+
+  let className = 'counter';
+  if (hover) {
+    className += ' hover';
+  }
+  if (isFancy) {
+    className += ' fancy';
+  }
+
+  return (
+    <div
+      className={className}
+      onPointerEnter={() => setHover(true)}
+      onPointerLeave={() => setHover(false)}
+    >
+      <h1>{score}</h1>
+      <button onClick={() => setScore(score + 1)}>
+        Add one
+      </button>
+    </div>
+  );
+}
